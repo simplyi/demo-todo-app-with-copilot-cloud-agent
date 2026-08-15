a7a31840799f84533f79ad3edcf4117145eaa358
simplyi <sergey.kargopolov@gmail.com>
2026-08-15 14:51:32 -0400

    first commit

---
+++ src/main/java/com/appsdeveloperblog/todoapp/DemoTodoAppWithCopilotCloudAgentApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTodoAppWithCopilotCloudAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTodoAppWithCopilotCloudAgentApplication.class, args);
+	}
+
+}
