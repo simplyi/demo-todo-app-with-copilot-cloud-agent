a1ec74f821fd7eb2d91e3889b9899debc9043ba6
simplyi <sergey.kargopolov@gmail.com>
2026-08-24 10:07:11 -0400

    first commit

---
+++ src/test/java/com/appsdeveloperblog/todoapp/DemoTodoAppWithCopilotCloudAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTodoAppWithCopilotCloudAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
